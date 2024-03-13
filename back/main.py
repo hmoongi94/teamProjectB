9681fac34d77b105c4a9cfc0774994962b57eedb
HongMoonGI <hmoongi94@gmail.com>
2024-03-13 17:20:21 +0900

    feat: 인터프리터 파이썬( 맞춤-> 전역)으로 변경하니 해결됨.

--- back/main.py
+++ back/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 오리진을 허용하도록 설정, 필요에 따라 변경 가능
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def printHello():
